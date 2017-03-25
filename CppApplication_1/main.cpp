f1ffae572d1e9916aabb02f1e65647f9945a27bd
Rosita <Rosita@Rosita>
2017-03-24 23:10:31 -0500

    Hola mundo

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    float precio = 0;
+    int tipoLibro = 0;
+    float porcentaje = '0.03';
+    
+    while(precio!=0){
+        cout<<"INGRESE EL PRECIO DEL LIBRO: "<<endl;
+        cin>>precio;
+        cout<<"INGRESE EL TIPO DE LIBRO (1=ACCION - 2=TERROR): "<<endl;
+        cin>>tipoLibro;
+        
+        printf("TIPO LIBRO SELECCIONADO: ", tipoLibro);
+        
+            
+    }    
+}
